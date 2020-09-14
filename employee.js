883a668ac2b8800fbaa6b494aabf128a30f37f60
frobbins <robbinsfatim@gmail.com>
2020-09-13 19:45:04 -0600

    Built a function to view all departments, roles and employees

--- employee.js
+++ employee.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -17,6 +18,40 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  queryAllEmployees();
+  runSearch();
 //   queryDanceSongs();
 });
+
+function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "exit"
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View all departments":
+          departmentSearch();
+          break;
+  
+        case "View all roles":
+          rolesSearch();
+          break;
+  
+        case "View all employees":
+          employeeSearch();
+          break;
+  
+        case "exit":
+          connection.end();
+          break;
+        }
+      });
+  }
