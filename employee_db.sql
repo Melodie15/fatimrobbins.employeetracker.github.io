47c791e80350b2522b543c2c16af30b1d369e165
frobbins <robbinsfatim@gmail.com>
2020-09-13 18:36:19 -0600

    Created employeelist, role and department tables

--- employee_db.sql
+++ employee_db.sql
@@ -5,3 +5,29 @@ CREATE DATABASE employee_db;
 
 -- Makes it so all of the following code will affect employee_db --
 USE employee_db;
+
+-- Creates the table "employeeList" within employee_db --
+CREATE TABLE employeelist (
+  id INT AUTO_INCREMENT NOT NULL,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INT NOT NULL,
+   manager_id INT,
+   PRIMARY KEY (id)
+
+);
+
+CREATE TABLE role (
+ id INT AUTO_INCREMENT NOT NULL,
+   title VARCHAR(30) NOT NULL,
+   salary DECIMAL(7,0) NOT NULL,
+   department_id INT NOT NULL,
+   PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL,
+  name VARCHAR(30) NOT NULL,
+   PRIMARY KEY (id)
+);
+
