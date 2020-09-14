5aad951d70277d42eb8f9f4e8186e7b33ef7a2fc
frobbins <robbinsfatim@gmail.com>
2020-09-13 18:44:52 -0600

    Created a seeds folder with department values

---
+++ seeds.sql
@@ -0,0 +1,13 @@
+USE employee_db;
+
+INSERT INTO department(name)
+VALUES ("Digital Marketing");
+
+INSERT INTO department(name)
+VALUES ("Software Engineering");
+
+INSERT INTO department(name)
+VALUES ("Web Development");
+
+INSERT INTO department(name)
+VALUES ("Web Design");
